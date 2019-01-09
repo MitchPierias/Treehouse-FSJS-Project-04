22796c8333fab08ee78e11cf5717f2cc5ca99944
Mitch Pierias <mitch@pierias.com>
2019-01-09 11:53:45 +1000

    Delegated event handling to the Game class

--- js/app.js
+++ js/app.js
@@ -15,16 +15,8 @@ document.getElementById('btn__reset').addEventListener('click', event => {
     game.startGame();
 });
 // Handle button element clicks
-document.getElementById('qwerty').addEventListener('click', ({ target }) => {
-    // Limit click target to button and handle button interactions
-    if (target.tagName != 'BUTTON' && target.className !== 'key') return;
-    game.handleInteraction(target);
-});
+document.getElementById('qwerty').addEventListener('click', game.handleInteraction);
 // Handle keyboard input
-document.addEventListener("keydown", ({ key }) => {
-    // Skip non single characters
-    if (!/^[a-z]{1}$/gi.test(key)) return;
-    console.log(key);
-}, false);
+document.addEventListener("keydown", game.handleInteraction, false);
 
 // App styles have been personalized and changes have been noted in the README.md file and the project submission notes
